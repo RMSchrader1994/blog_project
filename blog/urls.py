a15b6b245ac34175a8b23585eb63a155c5927e4f
Rebecca Schrader <rmschrader1994@gmail.com>
2018-03-14 12:44:02 +0000

    Edit and Delete Button Working

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     url(r'^posts/', post_list, name='post_list'),
     url(r'^create/', create_posts, name='create_posts'),
+    url(r'delete/(\d+)$', delete_post, name="delete_post"),
+    url(r'edit/(\d+)$', edit_post, name="edit_post"),
     url(r'^post/(\d+)', post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,11 @@ def post_detail(request, id):
     post = get_object_or_404(Post, pk=id)
     return render(request, "blog/post_detail.html", {'post': post})
     
+def delete_post(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.delete()
+    return redirect("post_list")
+    
 def create_posts(request):
     if request.method=="POST":
         form = PostItemForm(request.POST)
@@ -24,3 +29,18 @@ def create_posts(request):
     
     items = PostItemForm()
     return render(request, "blog/create.html", { 'form': form, 'items': items})
+    
+def edit_post(request, id): 
+    post = get_object_or_404(Post, pk=id)
+
+    if request.method == "POST":
+        print("It's the POST")
+        form = PostItemForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("post_list")
+    else:
+        print("It's the GET")
+        
+    form = PostItemForm(instance=post)
+    return render(request, "blog/create.html", {'form': form})
