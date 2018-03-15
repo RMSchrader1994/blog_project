b5a4b8022d7282c1216918bd7c2353f761c2b2db
Rebecca Schrader <rmschrader1994@gmail.com>
2018-03-15 16:03:49 +0000

    AWS2

--- blog_prj/settings/base.py
+++ blog_prj/settings/base.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
